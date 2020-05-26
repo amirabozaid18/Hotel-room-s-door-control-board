f6d1845d3fcc68a7097e4b8956a663d1e9841cc9
Amir Abozaid Abdelaziz <45122818+amirabozaid18@users.noreply.github.com>
2020-05-26 16:10:16 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,51 @@
+#include "C:\Keil\EE319Kware\inc\tm4c123gh6pm.h"
+#include "stdint.h"
+#include "string.h"
+
+void SystemInit(void){}
+void keypad_init(void);
+int put_char(char c,char* myinput);
+void solenoid_init(void);
+void UART1_init(void);	
+char getkey(void);
+	
+char operation[7] = {'\0','\0','\0','\0','\0','\0','\0'};
+char password[5] = {'\0','\0','\0','\0','\0'};
+unsigned char done = 0; // a flag to determine if UART sent the whole operation or still sending it
+
+int main(void)
+{
+	char c;
+	char myinput[5] = {'\0','\0','\0','\0','\0'}; //user input in case of checkin mode
+	solenoid_init();     //when solenoid is locked, red LED turns on, but when it's unlocked, green LED truns on
+	keypad_init();
+	UART1_init();
+	
+	while(1)      //setup mode , it ends when the uset enters 'T' for "Terminate"
+	{
+		c = UART1_DR_R & 0x0FF;
+		if (c == 'T')  {break; }
+	}
+	__enable_irq();
+	while (1)    // 3 possible scenarios each time : checkin(1), cleaning room(2), or checkout(0)
+	{
+		if (done)
+		{
+			if (operation[1] == '2') { GPIO_PORTF_DATA_R = (1<<3); }  //cleaning room
+			else if (operation[1] == '0' ) { GPIO_PORTF_DATA_R = (1<<1); }  //checkout
+			else if (operation[1] == '1' )   //checkin
+			{
+				GPIO_PORTA_DATA_R &= 0xF0;         // drive all rows to be low
+				if ( (GPIO_PORTA_DATA_R & 0xF0) != 0xF0)   // check if any column are low 
+				{
+					c = getkey();
+					if (put_char(c,myinput))
+					{ if (!strcmp(myinput,password)) { GPIO_PORTF_DATA_R = (1<<3);}
+						else {  GPIO_PORTF_DATA_R = (1<<1); }
+					}
+					while ( (GPIO_PORTA_DATA_R & 0xF0) != 0xF0);  // key is still not released
+				}
+			}
+		}
+	}
+}
